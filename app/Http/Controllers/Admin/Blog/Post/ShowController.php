deeed52b59739e574adbe5f227279fe9c8e019f9
AlexV500 <alexv55555@gmail.com>
2024-03-08 12:59:42 +0200

    Commit 8.03.2024

--- app/Http/Controllers/Admin/Blog/Post/ShowController.php
+++ app/Http/Controllers/Admin/Blog/Post/ShowController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin\Blog\Post;
 use App\Http\Controllers\Admin\Blog\AdminBlogController;
 use App\Models\Blog\Post;
 
-class ShowController extends AdminBlogController{
+class ShowController extends BaseController{
 
     public function __invoke(Post $post){
 
